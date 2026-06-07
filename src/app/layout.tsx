ccb87e22f71034020548e8fd5464c9943cd6bd85
reza <150424034+RezaChangizi75@users.noreply.github.com>
2026-06-07 11:27:13 +0330

    add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,30 @@
 import type { Metadata } from "next";
 import { Providers } from "./providers";
+import localFont from "next/font/local";
 import "./globals.css";
+
+
+const vazirmatn = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Vazirmatn-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Vazirmatn-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Vazirmatn-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-vazirmatn",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "سیوان لند | جزئیات محصول",
@@ -13,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fa" dir="rtl" className="h-full antialiased">
+    <html lang="fa" dir="rtl" className={`h-full antialiased ${vazirmatn.variable}`}>
       <body className="flex min-h-full flex-col">
         <Providers>{children}</Providers>
       </body>
